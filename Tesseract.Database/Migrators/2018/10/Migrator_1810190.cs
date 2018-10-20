cd325fd8a94bebf492743da3b072bf96725c70f1
Jordan Camara <jordycam@gmail.com>
2018-10-20 17:06:41 -0400

    Adding down migration.

--- Tesseract.Database/Migrators/2018/10/Migrator_1810190.cs
+++ Tesseract.Database/Migrators/2018/10/Migrator_1810190.cs
@@ -6,11 +6,16 @@ using FluentMigrator;
 
 namespace Tesseract.Database.Migrators._2018._10
 {
-    public class Migrator_1810190 : ForwardOnlyMigration
+    public class Migrator_1810190 : Migration
     {
         public override void Up()
         {
             Execute.EmbeddedScript("DatabaseSetup.sql");
+        }
+
+        public override void Down()
+        {
+            Execute.Sql("DROP DATABASE tesseract; DROP LOGIN TesseractUser");
         }
     }
 }
