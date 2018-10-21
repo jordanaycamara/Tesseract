8f5d1065be6366255f6c20b1c34d3aad294f7395
Jordan Camara <jordycam@gmail.com>
2018-10-20 21:49:08 -0400

    Adding database teardown.

---
+++ Tesseract.Database/Migrators/2018/10/DatabaseTeardown.sql
@@ -0,0 +1,6 @@
+﻿DROP TABLE Company.[Dependent];
+DROP TABLE Company.Employee;
+DROP TABLE Finances.Benefit;
+DROP TABLE Finances.BenefitType;
+DROP TABLE Finances.Discount;
+DROP TABLE Finances.DiscountType;
--- Tesseract.Database/Migrators/2018/10/Migration_1810200.cs
+++ Tesseract.Database/Migrators/2018/10/Migration_1810200.cs
@@ -6,11 +6,16 @@ using System.Text;
 namespace Tesseract.Database.Migrators._2018._10
 {
     [Migration(1810200, "Creating tables.")]
-    public class Migration_1810200 : ForwardOnlyMigration
+    public class Migration_1810200 : Migration
     {
         public override void Up()
         {
             Execute.EmbeddedScript("Tesseract.Database.Migrators._2018._10.DatabaseSetup.sql");
+        }
+
+        public override void Down()
+        {
+            Execute.EmbeddedScript("Tesseract.Database.Migrators._2018._10.DatabaseTeardown.sql");
         }
     }
 }
