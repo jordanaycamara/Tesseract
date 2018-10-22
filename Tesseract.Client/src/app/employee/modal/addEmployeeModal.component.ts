3eaec1159772d5409c129ec6fabf767865343aae
Jordan Camara <jordycam@gmail.com>
2018-10-22 01:10:05 -0400

    Fixing styles.

--- Tesseract.Client/src/app/employee/modal/addEmployeeModal.component.ts
+++ Tesseract.Client/src/app/employee/modal/addEmployeeModal.component.ts
@@ -4,7 +4,8 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'add-employee-modal',
-  templateUrl: './addEmployeeModal.component.html'
+  templateUrl: './addEmployeeModal.component.html',
+  styleUrls: ['./addEmployeeModal.component.scss']
 })
 
 export class AddEmployeeModalComponent {
