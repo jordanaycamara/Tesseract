cdddb0d7892ca01b6c66127996e20f347d03e43c
Jordan Camara <jcamara@autosoftdms.com>
2018-10-25 10:45:10 -0400

    Adding tests.

---
+++ Tesseract.Tests/Service/CostControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using Tesseract.Database.Commands.Finances;
+using Tesseract.Database.Models;
+using Tesseract.Service.Cost;
+
+namespace Tesseract.Tests.Service
+{
+    public class CostControllerTests
+    {
+        private CostController _controller;
+        private Mock<ICalculateCostsQuery> _mockQuery;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockQuery = new Mock<ICalculateCostsQuery>();
+            _mockQuery.Setup(x => x.CalculateCosts(It.IsAny<int>())).Returns(new CostReportViewModel());
+            _controller = new CostController(_mockQuery.Object);
+        }
+
+        [Test]
+        public void TestCalculateCosts()
+        {
+            var result = _controller.Get(1);
+
+            Assert.NotNull(result);
+            Assert.NotNull(result.Value);
+
+            _mockQuery.Verify(x => x.CalculateCosts(It.IsAny<int>()), Times.Once);
+        }
+    }
+}
